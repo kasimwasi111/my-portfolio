c64e89b224d58bb8e2eea4590e1a15c5e391b622
Mohaamad Kasim <kasimwasi111@gmail.com>
2023-06-28 01:44:36 +0530

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
-            </div>
-            <p className='text-xl mt-20'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur libero dignissimos magnam nostrum dicta at dolore accusamus aliquid sed, totam cumque excepturi placeat recusandae iure exercitationem delectus voluptate fuga beatae hic officiis non iusto assumenda minima. Non, laudantium nostrum impedit sit nemo tenetur dicta libero consectetur maxime magnam perspiciatis excepturi!
-            </p>
-            <br/>
-            <p className='text-xl'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum iusto necessitatibus adipisci rerum dolor deserunt cumque enim a temporibus incidunt! Maiores, voluptate quidem aliquam delectus cum dignissimos quo consequuntur quaerat.
-            </p>
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
+        <p className="text-xl mt-10">
+          Fresher with vast collection of tech stacks with good communication
+          skill. Searching for Front-end role in a decent company.I want to use
+          my knowledge and skills to gain practical exposure and understand the
+          inner-workings of the company. I’d like to learn on the job and grow
+          individually.
+        </p>
+        <br />
+        <p className="text-xl">
+          Secured 8.85 CGPA in Master of Computer Application (MCA) from MANUU
+          Hyderabad. Worked on several projects, solved 200+ DSA problems also
+          participated in various coding competition
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,66 +1,71 @@
-import React from 'react'
-import movieSearch from '../assets/portfolio/movieSearch.png';
-import qClock from '../assets/portfolio/qClock.png';
-import tipCalculator from '../assets/portfolio/tipCalculator.png';
+import React from "react";
+import movix from "../assets/portfolio/movix.png";
+import qClock from "../assets/portfolio/qClock.png";
+import recipeFinder from "../assets/portfolio/recipeFinder.png";
+import tipCalculator from "../assets/portfolio/tipCalculator.png";
 
 const Portfolio = () => {
-
-    const portfolios=[
-        {
-            id:1,
-            src:movieSearch,
-            demo:'https://movie-search-app-by-kasim-wasi.netlify.app/',
-            code:'https://github.com/kasimwasi111/movie-search'
-            
-        },
-        {
-            id:2,
-            src:qClock,
-            demo:'https://q-clock.netlify.app/',
-            code:'https://github.com/kasimwasi111/Analog-Clock'
-        },
-        {
-            id:3,
-            src:tipCalculator,
-            demo:'https://helpful-dolphin-352174.netlify.app/',
-            code:'https://github.com/kasimwasi111/https---github.com-kasimwasi111-Bill-Calculator'
-        },
-    ];
-    return (
-        <div
-          name="portfolio"
-          className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
-        >
-          <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-            <div className="pb-8">
-              <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-                Portfolio
-              </p>
-              <p className="py-6">Check out some of my work right here</p>
-            </div>
-    
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-              {portfolios.map(({ id, src,demo,code }) => (
-                <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-                  <img
-                    src={src}
-                    alt=""
-                    className="rounded-md duration-200 hover:scale-105"
-                  />
-                  <div className="flex items-center justify-center">
-                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    <a href={demo}>Demo</a>
-                    </button>
-                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    <a href={code}>Code</a>
-                    </button>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
+  const portfolios = [
+    {
+      id: 1,
+      src: movix,
+      demo: "https://movix-steel.vercel.app/",
+      code: "https://github.com/kasimwasi111/movix",
+    },
+    {
+      id: 2,
+      src: qClock,
+      demo: "https://q-clock.netlify.app/",
+      code: "https://github.com/kasimwasi111/Analog-Clock",
+    },
+    {
+      id: 3,
+      src: tipCalculator,
+      demo: "https://helpful-dolphin-352174.netlify.app/",
+      code: "https://github.com/kasimwasi111/https---github.com-kasimwasi111-Bill-Calculator",
+    },
+    {
+      id: 4,
+      src: recipeFinder,
+      demo: "https://recipe-gen.vercel.app/",
+      code: "https://github.com/kasimwasi111/recipe-gen",
+    },
+  ];
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
         </div>
-      );
-    };
 
-export default Portfolio
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, demo, code }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <a href={demo}>Demo</a>
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <a href={code}>Code</a>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
